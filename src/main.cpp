fb154fafb3c986750832462ea124c1d0071e348c
zqy <850074816@qq.com>
2018-07-22 00:43:47 +0800

    change tab

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ void output_entry(Entry entry)
 void output_entry1(Entry entry)
 {
     cout << entry.word;
-    int tab = 12;
+    int tab = 16;
     int l = tab - (entry.word.size()) % tab;
     for (int i = 0; i < l; i++)
         cout << " ";
